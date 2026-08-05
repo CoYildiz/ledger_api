dee6ac88e2cfdc4f8ed4dd6838a225b0b3a923a8
CoYildiz <yusufyildizr@gmail.com>
2026-08-05 19:38:30 +0300

    gorev1-done

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,8 @@
-from fastapi import FastAPI 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
     database_url: str
    
-
+    model_config = SettingsConfigDict(env_file=".env")
 settings = Settings()
-app = FastAPI()
